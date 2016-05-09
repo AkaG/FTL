36e6d31fa90b51120c688a0ad8ff49ea4f75bb82
AkaG <kuba0095@gmail.com>
2016-05-09 16:54:45 +0200

    Dzialajaca wersja

--- FTL.ino
+++ FTL.ino
@@ -1,34 +1,39 @@
-#define l_engine_speed 9 //analog pin controlling speed of left engine
-#define r_engine_speed 10 //analog pin controlling speed of right engine
+#define l_engine_speed 10 //analog pin controlling speed of left engine
+#define r_engine_speed 9 //analog pin controlling speed of right engine
 
-#define l_engine_dir1 3 // pin 1 controlling left engine
+#define l_engine_dir1 5 // pin 1 controlling left engine
 #define l_engine_dir2 4 // pin 2 controlling left engine
-#define r_engine_dir1 5 // pin 1 controlling right engine
-#define r_engine_dir2 6 // pin 2 controlling right engine
+#define r_engine_dir1 3 // pin 1 controlling right engine
+#define r_engine_dir2 2 // pin 2 controlling right engine
 
 #define changeColor 1 // switch black and white
 
-#define topSpeed 150 
-#define maxTurn 150
+#define topSpeed 190
+#define maxTurn 190
 
 int sensorPorts[] = {8, 6, 7, 13, 12}; //ports number of sensors
 int sensorCount = 5; 
 
-int kp = maxTurn/2; // proportional ratio
-int kd = kp/2; // derivative ratio
+int kp = maxTurn/6; // proportional ratio
+int kd = 60; // derivative ratio
 int ki = 1; // integral ratio
-int kiMax = (kp*2)/3; //max integral
+int kiMax = 60; //max integral
 
-int kiJump = 5; //integral increment
-int kiDelay = 50; // integral delay
+int kiJump = 1; //integral increment
+int kiSum = 0; // avg integral
+int kiDelay = 60; // integral delay
 int kiLoop = 0; // loop
 
-int kdDelay = 50; // derivative delay
+int kdDelay = 350; // derivative delay
+int kdSum = 0; // avg derivative
 int kdLoop = 0; // loop
 
 // calibration of engines
 int kalibracjaL = topSpeed; 
 int kalibracjaR = topSpeed;
+
+int zwolnij_m = 3;
+int zwolnij_d = 4;
 
 //start values
 int proportional = 0; 
@@ -60,15 +65,26 @@ void setup() {
 }
 
 void loop() {
+/*
+delay(100);
+for (int i = 0 ; i < 5 ; i++)
+  Serial.print(digitalRead(sensorPorts[i]));
+ Serial.println();
+*/
  // calculate int, der
   calcError();
   
   derivative = error - lastError;
   
 // integral delay
+
+  kiSum += error * kiJump;
   if(kiLoop >= kiDelay){
-    integral += error * kiJump;
+    integral += kiSum / kiDelay;
     kiLoop = 0;
+    if(kiSum == 0)
+      integral = integral / 2;
+    kiSum = 0;
   }
   kiLoop++;
 
@@ -103,15 +119,26 @@ void loop() {
    if(proportional == 0){
       ride(1, speedL, speedR);
    }else{
-      ride(1, speedL/2, speedR/2);
+      if(turn == -maxTurn){
+        ride(3,topSpeed,topSpeed);
+      }else{
+        if(turn == maxTurn){
+          ride(4,topSpeed,topSpeed);
+        }else{
+          ride(1, speedL*zwolnij_m/zwolnij_d, speedR*zwolnij_m/zwolnij_d);
+        }
+      }
    }
    
 // derivative delay
+   kdSum += error;
    if(kdLoop >= kdDelay){
-      lastError = error;
+      lastError = kdSum / kdDelay;
       kdLoop = 0;
+      kdSum = 0;
    }
-   kdLoop++;   
+   kdLoop++;  
+ 
 }
 
 void ride(int dir, int ftlSpeedl, int ftlSpeedr){ //0-stop, 1-forward, 2-backward, 3-FB, 4-BF
@@ -177,56 +204,29 @@ int mxor(int a, int b){
 
 void calcError(){
    int sum = 0;
-   int posLeft = 10;
-   int posRight = 10;
+   int pos = 0;
 
-   // left side
-   for(int i = sensorCount/2 - 1; i >= 0; i--){    //read left sensors
+   for(int i = 0; i  < sensorCount; i++){    //read left sensors
       sum += mxor(changeColor, digitalRead(sensorPorts[i])); // number of active left side sensors
       if(mxor(changeColor, digitalRead(sensorPorts[i])) == 1){ // which sensor is active
-        posLeft = i - sensorCount/2;
+        pos += (i - sensorCount/2)*2;
       }
    }
-
-   //right side
-   for(int i = sensorCount/2 + (sensorCount % 2); i < sensorCount; i++){   //read right sensors
-      sum += mxor(changeColor, digitalRead(sensorPorts[i])); // number of active right side sensors
-      if(mxor(changeColor, digitalRead(sensorPorts[i])) == 1){ // which sensor is active
-        posRight = i - sensorCount/2;
-      }
-   }
-
-
-   if(sum == 0){ // if no sensor are active
+   if(sum == 0){ // if no sensor is active
       if(lastError < 0){
-        error = -sensorCount;
+        error = -6;
       }else{
-        error = sensorCount;
+        error = 6;
       }
    }else{
-       if((posLeft != 10) && (posRight != 10)){ // if sensors are active on both sides
-         error = 0;
-       }
-       else{
-          if(posRight != 10){ //if sensors are active on right side
-            error = posRight;
-          }
-          else{
-            if(posLeft != 10){ //if sensors are active on left side
-              error = posLeft;
-            }
-          }
-       }
-   }
-
+       error = pos/sum;
+     }
+/*
     if(sensorCount % 2 == 1){ //if center sensor is active go straight on
      if(mxor(changeColor, digitalRead(sensorPorts[sensorCount/2])) == 1){
       error = 0;
      }
    }
-   
+ */ 
    proportional = error;
 }   
-   
-
-
